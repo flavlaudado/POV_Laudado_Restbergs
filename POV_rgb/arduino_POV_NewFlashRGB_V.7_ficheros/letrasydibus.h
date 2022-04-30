7c9f2bc35000a2d50047d266748e4d1618f10bc7
Flavia Laudado <flavialaudado@gmail.com>
2022-04-29 21:47:25 -0300

    arrays en ficheros, no funciona

---
+++ POV_rgb/arduino_POV_NewFlashRGB_V.7_ficheros/letrasydibus.h
@@ -0,0 +1,43 @@
+
+
+extern const boolean letterA[];
+extern const boolean letterB[];
+extern const boolean letterC[];
+extern const boolean letterD[];
+extern const boolean letterE[];
+extern const boolean letterF[];
+extern const boolean letterG[];
+extern const boolean letterH[];
+extern const boolean letterI[];
+extern const boolean letterJ[];
+extern const boolean letterK[];
+extern const boolean letterL[];
+extern const boolean letterM[];
+extern const boolean letterN[];
+extern const boolean letterO[];
+extern const boolean letterP[];
+extern const boolean letterQ[];
+extern const boolean letterR[];
+extern const boolean letterS[];
+extern const boolean letterT[];
+extern const boolean letterU[];
+extern const boolean letterV[];
+extern const boolean letterW[];
+extern const boolean letterX[];
+extern const boolean letterY[];
+extern const boolean letterZ[];
+extern const boolean letter33[];
+
+extern const boolean letter0[];
+extern const boolean letter1[];
+extern const boolean letter2[];
+extern const boolean letter3[];
+extern const boolean letter4[];
+extern const boolean letter5[];
+extern const boolean letter6[];
+extern const boolean letter7[];
+extern const boolean letter8[];
+extern const boolean letter9[];
+
+extern const boolean dibujo_a[];
+extern String dibujo_a_color;
